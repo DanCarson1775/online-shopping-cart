a58f572ffc5f8b783c6a6faed59bbf957af0da9d
DanCarson1775 <dancarson1775@gmail.com>
2026-01-06 18:07:32 -0800

    first update

---
+++ main.py
@@ -0,0 +1,21 @@
+"""
+Module 4 - Portfolio Milestone
+
+<DESCRIBE PROGRAM>
+
+<HOW PROGRAM WORKS>
+"""
+
+
+class ItemToPurchase:
+    """Represents item to be purchased based on item attributes and cost."""
+
+    def __init__(self):
+        """Default Constructor."""
+        self.item_name = ""
+        self.item_price = 0.0
+        self. item_quantity = 0
+
+    def print_item_cost():
+        """Calculates cost of item and prints to output."""
+        pass
